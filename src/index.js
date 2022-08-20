24daac1afc89d1c8c533d53f1191bb99f3bf70e4
Cedar <cedarlzp@163.com>
2022-08-20 13:30:15 +0800

    准备上传到github

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import x from './x.js'
 console.log(x)
 console.log('hi')
+
+
+import jpg from './assets/1.jpg'
+console.log(jpg)
+const div = document.querySelector('#app1')
+div.innerHTML = ` <img src="${jpg}"> `
+
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+div.appendChild(button)
+button.onclick = ()=>{
+    const promise = import('./lazy.js')
+    promise.then((module)=>{module.default()},()=>{console.log('或许是路径写错了')})
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,18 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const base = require('./webpack.config.base.js');
 
 module.exports = {
+  ...base,
   mode: 'development',
-
   devtool:'inline-source-map',
   devServer: {
     contentBase: './dist'
   },
-  
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[contenthash].js'
-  },
-  plugins: [new HtmlWebpackPlugin(
-    {
-    title: '我是package.json里面的title',
-    template: './src/assets/index.html'
-    }
-)],
 module: {
   rules: [
+    ...base.module.rules,
     {
       test: /\.css$/,
       use: [ 'style-loader', 'css-loader' ]
